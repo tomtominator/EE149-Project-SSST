a2c7746fb64adfb200778b15ba70938f2003ca02
Saransh Saini <66969478+saranshsaini@users.noreply.github.com>
2023-12-01 19:08:10 -0800

    Update esc_servo.ino

--- esc_servo/esc_servo.ino
+++ esc_servo/esc_servo.ino
@@ -1,10 +1,12 @@
 #include <Servo.h>
 
-byte servo1 = 3; // signal pin for the ESC.
+byte servo1 = 3;
 byte servo2 = 5;
 byte servo3 = 6;
 byte servo4 = 9;
 Servo esc1, esc2, esc3, esc4;
+
+int STOPSPEED = 1000;
 
 void setup() {
   esc1.attach(servo1);
@@ -15,20 +17,36 @@ void setup() {
 }
 
 void loop() {
-  bool ran = false;
-  if(!ran){
-    esc1.writeMicroseconds(1050);
-    esc2.writeMicroseconds(1050);
-    esc3.writeMicroseconds(1050);
-    esc4.writeMicroseconds(1050);
-    delay(2000);  // Run for 5 seconds
-  
+  int motor_speeds[5] = {2100, 1050, 1060, 1070, 1080};
+
+  for(int i =0; i<1; i++){
+    int speed = motor_speeds[i];
+
+    esc1.writeMicroseconds(speed);
+    esc2.writeMicroseconds(speed);
+    esc3.writeMicroseconds(speed);
+    esc4.writeMicroseconds(speed);
+    delay(4000);  // Run for some seconds
+
     // Stop the motor
-    esc1.writeMicroseconds(1000);
-    esc2.writeMicroseconds(1000);
-    esc3.writeMicroseconds(1000);
-    esc4.writeMicroseconds(1000);
+    esc1.writeMicroseconds(STOPSPEED);
+    esc2.writeMicroseconds(STOPSPEED);
+    esc3.writeMicroseconds(STOPSPEED);
+    esc4.writeMicroseconds(STOPSPEED);
     delay(1000); // Send signal to ESC.
-    ran = true;
+
   }
+
+  // Stop them just in case
+  esc1.writeMicroseconds(STOPSPEED);
+  esc2.writeMicroseconds(STOPSPEED);
+  esc3.writeMicroseconds(STOPSPEED);
+  esc4.writeMicroseconds(STOPSPEED);
+  delay(20000); // Send signal to ESC.
+
+  // Stop program execution
+  while(true);
+
+  
+
 }
