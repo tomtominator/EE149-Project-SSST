9a7a858d42871e7d4f8c896cc2f4226ec74aee79
Sean Yang <seancyang@berkeley.edu>
2023-12-09 17:45:19 -0800

    init

---
+++ gyro_angle_test/gyro_angle_test.ino
@@ -0,0 +1,117 @@
+#include "FastIMU.h"
+#include <Wire.h>
+
+#define IMU_ADDRESS 0x68    //Change to the address of the IMU
+#define PERFORM_CALIBRATION //Comment to disable startup calibration
+MPU9250 IMU;               //Change to the name of any supported IMU! 
+
+
+calData calib = { 0 };  //Calibration data
+AccelData accelData;    //Sensor data
+GyroData gyroData;
+MagData magData;
+
+float curr;
+float prev;
+float temp_x;
+
+void start_imu(){
+  Serial.begin(115200);
+  Serial.println("STARTING");
+  
+  Wire.begin();
+  Wire.setClock(400000); //400khz clock
+
+  temp_x = 0;
+  prev = millis();
+  curr = millis();
+
+  int err = IMU.init(calib, IMU_ADDRESS);
+  delay(1000);
+  if (err != 0) {
+    Serial.println("IMU ERROR");
+    // digitalWrite(LED3, HIGH);
+    // digitalWrite(LED5, HIGH);
+    // digitalWrite(LED6, HIGH);
+    // digitalWrite(LED9, HIGH);
+    while (true);
+  }
+
+  if (IMU.hasMagnetometer()) {
+    // delay(1000);
+    // Serial.println("Move IMU in figure 8 pattern until done.");
+    delay(1000);
+    IMU.calibrateMag(&calib);
+    // Serial.println("Magnetic calibration done!");
+  }
+  else {
+    Serial.println("No magnometer");
+    delay(1000);
+  }
+  delay(1000);
+  IMU.calibrateAccelGyro(&calib);
+  delay(1000);
+  IMU.init(calib, IMU_ADDRESS);
+
+}
+
+void setup(){
+  start_imu();
+}
+
+void loop(){
+  imu_loop();
+}
+
+void imu_loop() {
+  Serial.print("accelX, accelY, accelZ, gyroX, gyroY, gyroZ \n");
+  while(true){
+    IMU.update();
+    IMU.getAccel(&accelData);
+    Serial.print(accelData.accelX);
+    Serial.print("\t");
+    Serial.print(accelData.accelY);
+    Serial.print("\t");
+    Serial.print(accelData.accelZ);
+    Serial.print("\t");
+    IMU.getGyro(&gyroData);
+    Serial.print(gyroData.gyroX);
+    Serial.print("\t");
+    Serial.print(gyroData.gyroY);
+    Serial.print("\t");
+    Serial.print(gyroData.gyroZ);
+    Serial.print("\n");
+    // if (IMU.hasMagnetometer()) {
+    //   IMU.getMag(&magData);
+    //   Serial.print("\t");
+    //   Serial.print(magData.magX);
+    //   Serial.print("\t");
+    //   Serial.print(magData.magY);
+    //   Serial.print("\t");
+    //   Serial.print(magData.magZ);
+    // }
+    // if (IMU.hasTemperature()) {
+    //   Serial.print("\t");
+    //   Serial.println(IMU.getTemp());
+    // }
+    IMU.getGyro(&gyroData);
+    float curr_x = gyroData.gyroX;
+    float diff = curr_x - temp_x;
+    curr = millis();
+    float time_elapsed = curr - prev;
+    float angle = diff / time_elapsed * 1000;
+    Serial.print(temp_x);
+    Serial.print("\t");
+    Serial.print(curr_x);
+    Serial.print("\t");
+    Serial.print(diff);
+    Serial.print("\t");
+    Serial.print(angle);
+    Serial.print("\n");
+
+    prev = curr;
+    temp_x = gyroData.gyroX;
+
+    delay(1000);
+  }
+}
---
+++ main/main.ino
@@ -0,0 +1,226 @@
+#include <Servo.h>
+#include "FastIMU.h"
+#include <Wire.h>
+MPU9250 IMU;
+byte servo3 = 3;
+byte servo5 = 5;
+byte servo6 = 6;
+byte servo9 = 9;
+Servo esc3, esc5, esc6, esc9;
+AccelData accelData;
+
+int LED3 = 10;
+int LED5 = 11;
+int LED6 = 12;
+int LED9 = 13;
+
+
+int STOP = 1000;
+int STEADY = 1050;
+int FASTER = 1070;//1020;
+int SLOWER = 1010;
+
+
+int TARGETX = -0.01;
+int TARGETY = 0.04;
+int TARGETZ = -1.00;
+
+unsigned long START_TIME;
+unsigned long RUNTIME = 5000;
+
+//Directions named off of which propellors need increased speed to stabilize drone
+enum DIRECTION {
+  D3,
+  D5,
+  D6,
+  D9,
+  D39,
+  D59,
+  D36,
+  D56,
+  D3569
+};
+
+static inline char *propellerName(enum DIRECTION dir)
+{
+    static const char *strings[] = { 
+      "D3",
+      "D5",
+      "D6",
+      "D9",
+      "D39",
+      "D59",
+      "D36",
+      "D56",
+      "D3569" 
+  };
+
+    return strings[dir];
+}
+
+void setup() {
+  // Serial.begin(115200);
+  pinMode(LED3, OUTPUT);
+  pinMode(LED5, OUTPUT);
+  pinMode(LED6, OUTPUT);
+  pinMode(LED9, OUTPUT);
+  // digitalWrite(LED3, HIGH);
+  digitalWrite(LED5, HIGH);
+  digitalWrite(LED6, HIGH);
+  // digitalWrite(LED9, HIGH);
+  // delay(1000);
+  
+
+  // while (true)
+  //   ;
+
+  start_imu();
+  // digitalWrite(LED3, LOW);
+  digitalWrite(LED5, LOW);
+  digitalWrite(LED6, LOW);
+  // digitalWrite(LED9, LOW);
+  // delay(1000);
+  // setup_imu();
+  // esc3.attach(servo3);
+  // esc5.attach(servo5);
+  // esc6.attach(servo6);
+  // esc9.attach(servo9);
+  // delay(10000);  // delay to allow the ESC to recognize the stopped signal.
+  START_TIME = millis();
+}
+
+
+
+void loop() {
+  IMU.update();
+  IMU.getAccel(&accelData);
+  float accelX = accelData.accelX;
+  float accelY = accelData.accelY;
+  
+  float accel_diff = abs(abs(accelX) - abs(accelY));
+  bool one_proppelor_on = false; //accel_diff>=0.8;
+  DIRECTION dir = D3569;
+  if (abs(accelX) <=0.03 && abs(accelY)<=0.03){
+    dir = D3569;
+  }
+  else if (accelX <= 0 && accelY <= 0) {
+    if (one_proppelor_on) {
+      dir = D6;
+    } else {
+      dir = D56;
+    }
+  } else if (accelX >= 0 && accelY >= 0) {
+    if (one_proppelor_on) {
+      dir = D9;
+    } else {
+      dir = D39;
+    }
+  } else if (accelX >= 0 && accelY <= 0) {
+    if (one_proppelor_on) {
+      dir = D3;
+    } else {
+      dir = D36;
+    }
+  } else if (accelX <= 0 && accelY >= 0) {
+    if (one_proppelor_on) {
+      dir = D5;
+    } else {
+      dir = D59;
+    }
+  }
+  // Serial.print("X, Y, DIR, accel_diff: ");
+  // Serial.print(accelX);
+  // Serial.print(", ");
+  // Serial.print(accelY);
+  // Serial.print(", ");
+  // Serial.print(propellerName(dir));
+  // Serial.print(", ");
+  // Serial.print(accel_diff);
+  // Serial.print("\n");
+
+
+  switch (dir) {
+    case D3:
+      esc3.writeMicroseconds(FASTER);
+      // digitalWrite(LED3, HIGH);
+      break;
+    case D5:
+      esc5.writeMicroseconds(FASTER);
+      // digitalWrite(LED5, HIGH);
+      break;
+    case D6:
+      esc6.writeMicroseconds(FASTER);
+      // digitalWrite(LED6, HIGH);
+      break;
+    case D9:
+      esc9.writeMicroseconds(FASTER);
+      // digitalWrite(LED9, HIGH);
+      break;
+    case D39:
+      esc3.writeMicroseconds(FASTER);
+      esc9.writeMicroseconds(FASTER);
+      // digitalWrite(LED3, HIGH);
+      // digitalWrite(LED9, HIGH);
+      break;
+    case D59:
+      esc5.writeMicroseconds(FASTER);
+      esc9.writeMicroseconds(FASTER);
+      // digitalWrite(LED5, HIGH);
+      // digitalWrite(LED9, HIGH);
+      break;
+    case D36:
+      esc3.writeMicroseconds(FASTER);
+      esc6.writeMicroseconds(FASTER);
+      // digitalWrite(LED3, HIGH);
+      // digitalWrite(LED6, HIGH);
+      break;
+    case D56:
+      esc5.writeMicroseconds(FASTER);
+      esc6.writeMicroseconds(FASTER);
+      // digitalWrite(LED5, HIGH);
+      // digitalWrite(LED6, HIGH);
+      break;
+    case D3569:
+      esc3.writeMicroseconds(STEADY);
+      esc5.writeMicroseconds(STEADY);
+      esc6.writeMicroseconds(STEADY);
+      esc9.writeMicroseconds(STEADY);
+      // digitalWrite(LED3, HIGH);
+      // digitalWrite(LED5, HIGH);
+      // digitalWrite(LED6, HIGH);
+      // digitalWrite(LED9, HIGH);
+      
+      break;
+
+  }
+  delay(2000);
+  esc3.writeMicroseconds(STEADY);
+  esc5.writeMicroseconds(STEADY);
+  esc6.writeMicroseconds(STEADY);
+  esc9.writeMicroseconds(STEADY);
+  // digitalWrite(LED3, LOW);
+  // digitalWrite(LED5, LOW);
+  // digitalWrite(LED6, LOW);
+  // digitalWrite(LED9, LOW);
+
+  // delay(1000);
+  if (millis() - START_TIME >= RUNTIME) {
+    stop_drone();
+  }
+}
+
+void stop_drone() {
+  esc3.writeMicroseconds(STOP);
+  esc5.writeMicroseconds(STOP);
+  esc6.writeMicroseconds(STOP);
+  esc9.writeMicroseconds(STOP);
+  digitalWrite(LED3, HIGH);
+  // digitalWrite(LED5, HIGH);
+  // digitalWrite(LED6, HIGH);
+  digitalWrite(LED9, HIGH);
+  delay(10000);  // Send signal to ESC.
+
+  // Stop program execution
+  while (true)
+    ;
+}
