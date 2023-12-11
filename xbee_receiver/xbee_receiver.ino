886b57be0afe18503e760fbb7dfb00b5f72bf173
Sean Yang <seancyang@berkeley.edu>
2023-12-11 00:49:28 -0800

    xbee + serial print works
    ENDDEVICE1 sends 1 to COORDINATOR1

--- xbee_receiver/xbee_receiver.ino
+++ xbee_receiver/xbee_receiver.ino
@@ -1,8 +1,8 @@
 #include <SoftwareSerial.h>
 
 // Define the XBee module's RX and TX pins
-const int xbee_Rx = 0;  // Connect XBee's TX to Arduino's D2
-const int xbee_Tx = 1;  // Connect XBee's RX to Arduino's D3
+const int xbee_Rx = 0;  // Connect XBee's TX to Arduino's pin 0
+const int xbee_Tx = 1;  // Connect XBee's RX to Arduino's pin 1
 
 SoftwareSerial Xbee(xbee_Rx, xbee_Tx);
 
@@ -14,12 +14,15 @@ void setup() {
 }
 
 void loop() {
-  if (Serial.available()) { 
-    Serial.print("hahahaha");
-    Xbee.write(Serial.read());
+  if (Xbee.available()) {
+    char data = Xbee.read();
+    Serial.print("Received from XBee: ");
+    Serial.println(data);
   }
-  if (Xbee.available()) { 
-    Serial.print("hohohohoho");
-    Serial.write(Xbee.read());
+  
+  if (Serial.available()) { 
+    char data = Serial.read();
+    Serial.print("Received from Serial Monitor: ");
+    Serial.println(data);
   }
 }
