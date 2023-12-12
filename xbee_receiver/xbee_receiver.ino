28a2c1949289f2479854f049d49ebf17816e01bb
Sean Yang <seancyang@berkeley.edu>
2023-12-12 02:48:32 -0800

    pins 2,3 for serial

--- xbee_receiver/xbee_receiver.ino
+++ xbee_receiver/xbee_receiver.ino
@@ -1,28 +1,28 @@
 #include <SoftwareSerial.h>
 
 // Define the XBee module's RX and TX pins
-const int xbee_Rx = 0;  // Connect XBee's TX to Arduino's pin 0
-const int xbee_Tx = 1;  // Connect XBee's RX to Arduino's pin 1
+const int xbee_Rx = 2;  // Connect XBee's TX to Arduino's pin 2
+const int xbee_Tx = 3;  // Connect XBee's RX to Arduino's pin 3
 
 SoftwareSerial Xbee(xbee_Rx, xbee_Tx);
 
 void setup() {
   Serial.begin(9600);
   Xbee.begin(9600);
-  Serial.print("Init");
-  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.println("Receiver Init");
 }
 
 void loop() {
   if (Xbee.available()) {
     char data = Xbee.read();
-    Serial.print("Received from XBee: ");
+    Serial.print("XBee: ");
     Serial.println(data);
-  }
-  
-  if (Serial.available()) { 
-    char data = Serial.read();
-    Serial.print("Received from Serial Monitor: ");
-    Serial.println(data);
+
+    if (data == '1') {
+      Serial.print("11111");
+    }
+    if (data == '2') {
+      Serial.print("11111");
+    }
   }
 }
