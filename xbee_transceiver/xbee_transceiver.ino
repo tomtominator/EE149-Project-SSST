ddc6d257f62d57d344c63fa953550b0b02462a24
Sean Yang <seancyang@berkeley.edu>
2023-12-11 23:05:41 -0800

    working transceiver

--- xbee_transceiver/xbee_transceiver.ino
+++ xbee_transceiver/xbee_transceiver.ino
@@ -1,28 +1,30 @@
 #include <SoftwareSerial.h>
 
 // Define the XBee module's RX and TX pins
-const int xbee_Rx = 0;  // Connect XBee's TX to Arduino's pin 0
-const int xbee_Tx = 1;  // Connect XBee's RX to Arduino's pin 1
+const int xbee_Rx = 2;  // Connect XBee's TX to Arduino's pin 0
+const int xbee_Tx = 3;  // Connect XBee's RX to Arduino's pin 1
 
 SoftwareSerial Xbee(xbee_Rx, xbee_Tx);
 
 void setup() {
-  Serial.begin(9600);
-  Xbee.begin(9600);
-  Serial.print("Init");
-  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200);
+  Xbee.begin(115200);
+  Serial.println("Transmitter Init");
 }
 
 void loop() {
-  if (Xbee.available()) {
-    char data = Xbee.read();
-    Serial.print("Received from XBee: ");
-    Serial.println(data);
-  }
+  // Send a message to the XBee receiver
+  sendMessage("Hello from XBee Transmitter");
+  if (Serial.parseInt())
+
+  // Wait for a moment before sending the next message
+  delay(2000);
+}
+
+void sendMessage(const char* message) {
+  Serial.print("Sending message: ");
+  Serial.println(message);
   
-  if (Serial.available()) { 
-    char data = Serial.read();
-    Serial.print("Received from Serial Monitor: ");
-    Serial.println(data);
-  }
+  Xbee.print(message);
+  Xbee.print('\n'); // Add a newline character to indicate the end of the message
 }
